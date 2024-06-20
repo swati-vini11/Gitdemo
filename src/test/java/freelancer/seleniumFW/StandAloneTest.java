ff864b4cdac31ad48f53689f4c4c30253a3abe26
swati-vini11 <155066922+swati-vini11@users.noreply.github.com>
2024-06-20 13:23:52 -0700

    Update StandAloneTest.java
    added comment

--- src/test/java/freelancer/seleniumFW/StandAloneTest.java
+++ src/test/java/freelancer/seleniumFW/StandAloneTest.java
@@ -38,7 +38,7 @@ public class StandAloneTest {
 		//w.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
 	
 		w.get("https://rahulshettyacademy.com/client");	
-		System.out.println("Successfully login******");
+		//System.out.println("Successfully login******");
 		
 		w.findElement(By.id("userEmail")).sendKeys("swati.vini11@gmail.com");
 		w.findElement(By.id("userPassword")).sendKeys("Rishabh27!");
