c0f37e26698e42d838e3e5c69ff20efe5f4a88a8
swati-vini11 <155066922+swati-vini11@users.noreply.github.com>
2024-06-20 23:16:41 -0700

    Update StandAloneTest.java

--- src/test/java/freelancer/seleniumFW/StandAloneTest.java
+++ src/test/java/freelancer/seleniumFW/StandAloneTest.java
@@ -11,9 +11,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
+//import org.openqa.selenium.By;
+//import org.openqa.selenium.JavascriptExecutor;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
